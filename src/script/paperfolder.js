f9e80646a19563753a637e8d2e5796902a979332
陈 亮亮 <chenllos@163.com>
2013-07-10 10:20:34 +0800

    参照stackoverflow上的公式，折纸效果达成~ 缺陷： 1.全是以自身为stage和设置perspective； 2.语言还不够优化，但是语义很强； 3.精确度由于页面渲染的问题还有待提升（小数点的px不被渲染，导致有细小缝隙）

---
+++ src/script/paperfolder.js
@@ -0,0 +1,79 @@
+( function(){
+	var P = 640;
+	var W = 200;
+	var ANG = 45;
+	function calc(oldx, angle, p){
+	    var x =Math.cos(angle)* oldx;
+	    var z =Math.sin(angle)* oldx;
+	    return x * p /(p+z);
+	}
+
+	function cacuD( oldD, w, angle, p ){
+		return p*( Math.abs(oldD) + w/2 -Math.cos( angle*Math.PI/180 ) ) / ( p + Math.sin( angle*Math.PI/180 )*w/2 );
+	}
+
+	function cacuC( oldC, w, angle, p ){
+		return p*( Math.abs(oldC) - w/2 + Math.cos( angle*Math.PI/180 ) ) / ( p - Math.sin( angle*Math.PI/180 ) * w/2 );
+	}
+	function changeAngle( nodeObj, transStr){
+		// var setting = 'perspective(' + P +') ';
+		nodeObj.style.webkitTransform = transStr;
+	}
+
+
+	function initTransDeg( nodeList, deg ){
+		var nodeCount = nodeList.length;
+		var defaultLoca = 0;
+		var angle;
+		defaultTrans = 'perspective(640) ';
+
+		for( var i = 0; i< nodeCount; i++ ){
+			if( i%2 == 0){
+				angle = deg;
+			}
+			else{
+				angle = -deg;
+			}
+			nodeList[i].style.webkitTransform = defaultTrans + 'rotateY(' + angle + 'deg)';
+			nodeList[i].style.left = i*25 + '%';
+		}
+	}
+
+	function moveToRightPos( nodeList ){
+		var oldA = 0;
+		var oldB = W;
+		var cordA, cordB;
+		var newA, newB;
+		/* -- 挪到中点坐标 -- */
+		oldA -= W/2;
+		oldB -= W/2;
+		/* -- 使用StackOverflow给出的公式计算 -- */
+		cordA = calc( oldA, ANG * (Math.PI/180), P );
+		cordB = calc( oldB, ANG * (Math.PI/180), P );
+		/* -- 还原坐标 -- */
+		newA = cordA + W/2;
+		newB = cordB + W/2;
+		console.log( 'coord: ' + cordA + ' -- ' + cordB );
+		console.log( 'new: ' + newA + ' -- ' + newB );
+
+		var realWidth = newB - newA;
+
+
+		for( var j = 0; j < nodeList.length; j++){
+			var toLeft = j * realWidth;
+			if( j%2 == 0 ){
+				nodeList[j].style.left = toLeft - newA + 'px';
+			}
+			else{
+				nodeList[j].style.left = toLeft + ( -(W - newB) ) + 'px';
+			}
+		}
+	}
+
+	window.onload = function(){
+		var transList = document.querySelectorAll('.ctn .trans');
+
+		initTransDeg( transList, ANG);
+		moveToRightPos( transList );
+	}
+})();
