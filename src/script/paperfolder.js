178a767d69dcc984b82cf17da6573baa9f73a4dd
陈 亮亮 <chenllos@163.com>
2013-07-30 15:50:47 +0800

    no message

--- src/script/paperfolder.js
+++ src/script/paperfolder.js
@@ -4,6 +4,8 @@
 	var ANG = 0;
 	var transList;
 
+	var persStyleStr = 'perspective('+P+'px)';
+
 
 	function calc(oldx, angle, p){
 	    var x =Math.cos(angle)* oldx;
@@ -29,7 +31,6 @@
 		var defaultLoca = 0;
 		var angle;
 		defaultTrans = 'perspective(640) ';
-
 		for( var i = 0; i< nodeCount; i++ ){
 			if( i%2 == 0){
 				angle = deg;
@@ -72,6 +73,36 @@
 				nodeList[j].style.left = toLeft + ( -(W - newB) ) + 'px';
 			}
 		}
+	}
+
+	function changeDegAndPosition( transList, angle){
+		// 由于现在使用的是以自身元素为舞台容器
+		// 所以现在的每个角度定下来的A和B都是固定的
+
+		// 现在先算出angle对应的AB来 
+		var A=0, B=W;
+
+		A -= W/2;
+		B -= W/2;
+
+		A = calc( A, angle * (Math.PI/180), P );
+		B = calc( B, angle * (Math.PI/180), P );
+
+		A += W/2;
+		B += W/2;
+
+		// 再给每个元素变形
+		// 比较精细的算法是在rotate一个元素后立刻对所有的元素进行位移 每一帧复杂度： 元素个数^2
+		// 而比较性能优化的方法是： rotate全部元素， 然后位移全部元素. 帧复杂度：元素个数*2
+		var nodeCount = transList.length;
+		var rotateStyleStr = ' rotateY('+angle+'deg)';
+
+		for( var i=0; i< nodeCount; i++){
+			nodeList[i].style.webkitTransform = persStyleStr + rotateStyleStr;
+			nodeList[i].style.left = 
+		}
+
+
 	}
 
 	function slowMotion( options ){
